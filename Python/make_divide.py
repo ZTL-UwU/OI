8bdcbc55eff9bd87585f18888a06edf7cad95270
zhangtianli2006 <49156174+zhangtianli2006@users.noreply.github.com>
2020-07-18 10:14:04 +0800

    7-18

--- Python/make_divide.py
+++
@@ -1,5 +0,0 @@
-import random
-a = int(random.uniform(1, 8951809851034598))
-c = int(a / 2)
-print(a)
-print(c)
--- Python/make_minus.py
+++
@@ -1,7 +0,0 @@
-import random
-a = int(random.uniform(1, 9854317871975))
-b = a + int(random.uniform(1, 98541975))
-c = b - a
-print(b)
-print(a)
-print(c)
