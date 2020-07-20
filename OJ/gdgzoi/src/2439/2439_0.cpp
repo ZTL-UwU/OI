6c0f47647e87bb43ac14a455486a0fa67fa050cc
zhangtianli2006 <49156174+zhangtianli2006@users.noreply.github.com>
2020-07-20 19:26:17 +0800

    7-20

--- OJ/gdgzoi/src/2439/2439_0.cpp
+++ OJ/gdgzoi/src/2439/2439_0.cpp
@@ -1,28 +1,24 @@
 #include <iostream>
-#include <vector>
 #include <string>
+#include <vector>
 using namespace std;
-class huge_int
-{
-private:
+class huge_int {
+    public:
     string bits;
-public:
-    void flip(string &a)
-    {
+    void flip(string &a) {
         for (int i = 0; i < a.size() / 2; i++)
             swap(a[i], a[a.size() - i - 1]);
     }
-    huge_int operator+(huge_int x)
-    {
+    huge_int operator+(huge_int x) {
         huge_int res;
         string a = this->bits, b = x.bits;
         if (b.size() > a.size()) swap(a, b);
         res.bits = a;
-        while (a.size() - b.size()) b = "0" + b;
+        while (a.size() - b.size())
+            b = "0" + b;
         flip(a), flip(b);
         int carry = 0;
-        for (int i = 0; i < a.size(); i++)
-        {
+        for (int i = 0; i < a.size(); i++) {
             int bit = a[i] + b[i] - 2 * '0' + carry;
             res.bits[i] = (bit % 10) + '0';
             carry = bit / 10;
@@ -31,15 +27,15 @@ public:
         flip(res.bits);
         return res;
     }
-    int operator[](int x) { return this->bits[x] - '0'; }
-    friend ostream &operator<<(ostream &os, const huge_int &x)
-    {
+    int operator[](int x) {
+        return this->bits[x] - '0';
+    }
+    friend ostream &operator<<(ostream &os, const huge_int &x) {
         os << x.bits;
         return os;
     }
 };
-int main()
-{
+int main() {
     int n, m;
     cin >> n >> m;
 
