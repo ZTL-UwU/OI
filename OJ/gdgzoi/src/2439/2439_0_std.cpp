5c371509db932f46ede06d89d3820e15cdf96754
zhangtianli2006 <49156174+zhangtianli2006@users.noreply.github.com>
2020-07-20 18:54:46 +0800

    7-20

--- OJ/gdgzoi/src/2439/2439_0_std.cpp
+++ OJ/gdgzoi/src/2439/2439_0_std.cpp
@@ -1,178 +1,127 @@
-#include <bits/stdc++.h>
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+#include <cmath>
 using namespace std;
-const int databit = 1e9;
-const int MOD = 2601;
+typedef long long LL;
+const int cube = (int)1e9, mod = 2601;
 int n, m;
-struct data
+struct Data_Analysis
 {
     int bit[6];
-    inline void clear()
+    inline void Clear() { memset(bit, 0, sizeof(bit)); }
+    Data_Analysis() { Clear(); }
+    inline void Set(int t)
     {
-        memset(bit, 0, sizeof(bit));
+        Clear();
+        while (t)
+            bit[++bit[0]] = t % cube, t /= cube;
     }
-    inline void set(int x)
+    inline int &operator[](int x) { return bit[x]; }
+    inline void Print()
     {
-        clear();
-        while (x)
-        {
-            bit[++bit[0]] = x % databit;
-            x /= databit;
-        }
+        printf("%d", bit[bit[0]]);
+        for (int i = bit[0] - 1; i > 0; i--)
+            printf("%09d", bit[i]);
+        printf("\n");
     }
-    inline int &operator[](int x)
+    inline Data_Analysis operator+(Data_Analysis b)
     {
-        return bit[x];
-    }
-    inline data operator+(data b)
-    {
-        data c;
-        c.clear();
+        Data_Analysis c;
+        c.Clear();
         c[0] = max(bit[0], b[0]) + 1;
         for (int i = 1; i <= c[0]; i++)
-            c[i] += bit[i] + b[i], c[i + 1] += c[i] / databit, c[i] %= databit;
+            c[i] += bit[i] + b[i], c[i + 1] += c[i] / cube, c[i] %= cube;
         while (!c[c[0]])
             c[0]--;
         return c;
     }
-    inline void operator+=(data b)
-    {
-        *this = *this + b;
-    }
-    inline void operator=(int x)
-    {
-        set(x);
-    }
-    inline void print()
-    {
-        printf("%d", bit[bit[0]]);
-        for (int i = bit[0] - 1; i >= 1; i--)
-            printf("%09d", bit[i]);
-        puts("");
-    }
-    data()
-    {
-        clear();
-    }
-};
-data ans;
-struct hashmp
+    inline void operator+=(Data_Analysis b) { *this = *this + b; }
+    inline void operator=(int x) { Set(x); }
+} Ans;
+struct Hash_Sheet
 {
-    data val[MOD];
-    int key[MOD], sz, hash[MOD];
-    inline void init()
+    Data_Analysis val[mod];
+    int key[mod], size, hash[mod];
+    inline void Initialize()
     {
-        memset(val, 0, sizeof(val));
-        memset(key, -1, sizeof(key));
-        sz = 0;
-        memset(hash, 0, sizeof(hash));
+        memset(val, 0, sizeof(val)), memset(key, -1, sizeof(key));
+        size = 0, memset(hash, 0, sizeof(hash));
     }
-    inline void newhash(int id, int v)
+    inline void Newhash(int id, int v) { hash[id] = ++size, key[size] = v; }
+    Data_Analysis &operator[](const int State)
     {
-        hash[id] = ++sz;
-        key[sz] = v;
-    }
-    inline data &operator[](int state)
-    {
-        for (int i = state % MOD;; i = (i + 1 == MOD) ? 0 : i + 1)
+        for (int i = State % mod;; i = (i + 1 == mod) ? 0 : i + 1)
         {
             if (!hash[i])
-                newhash(i, state);
-            if (key[hash[i]] == state)
+                Newhash(i, State);
+            if (key[hash[i]] == State)
                 return val[hash[i]];
         }
     }
-};
-hashmp dp[2];
-inline int find(int state, int id)
-{
-    return (state >> ((id - 1) << 1)) & 3;
-}
-inline void dpset(int &state, int bit, int val)
+} f[2];
+inline int Find(int State, int id) { return (State >> ((id - 1) << 1)) & 3; }
+inline void Set(int &State, int bit, int val)
 {
     bit = (bit - 1) << 1;
-    state |= 3 << bit;
-    state ^= 3 << bit;
-    state |= val << bit;
+    State |= 3 << bit, State ^= 3 << bit, State |= val << bit;
 }
-inline int link(int state, int pos)
+inline int Link(int State, int pos)
 {
-    int cnt = 0, delta = (find(state, pos) == 1) ? 1 : -1;
-    for (int i = pos; i && i <= m + 1; i += delta)
+    int cnt = 0, Delta = (Find(State, pos) == 1) ? 1 : -1;
+    for (int i = pos; i && i <= m + 1; i += Delta)
     {
-        int p = find(state, i);
-        if (p == 1)
+        int plug = Find(State, i);
+        if (plug == 1)
             cnt++;
-        else if (p == 2)
+        else if (plug == 2)
             cnt--;
         if (cnt == 0)
             return i;
     }
     return -1;
 }
-inline void run(int x, int y)
+inline void Execution(int x, int y)
 {
-    int now = ((x - 1) * m + y) & 1, last = now ^ 1, tot = dp[last].sz;
-    dp[now].init();
+    int now = ((x - 1) * m + y) & 1, last = now ^ 1, tot = f[last].size;
+    f[now].Initialize();
     for (int i = 1; i <= tot; i++)
     {
-        int state = dp[last].key[i];
-        data val = dp[last].val[i];
-        int p1 = find(state, y), p2 = find(state, y + 1);
-        if (link(state, y) == -1 || link(state, y + 1) == -1)
+        int State = f[last].key[i];
+        Data_Analysis Val = f[last].val[i];
+        int plug1 = Find(State, y), plug2 = Find(State, y + 1);
+        if (Link(State, y) == -1 || Link(State, y + 1) == -1)
             continue;
-        if (!p1 && !p2 && x != n && y != m)
+        if (!plug1 && !plug2)
         {
-            dpset(state, y, 1);
-            dpset(state, y + 1, 2);
-            dp[now][state] += val;
+            if (x != n && y != m)
+                Set(State, y, 1), Set(State, y + 1, 2), f[now][State] += Val;
         }
-        else if (p1 && !p2)
+        else if (plug1 && !plug2)
         {
             if (x != n)
-                dp[now][state] += val;
+                f[now][State] += Val;
             if (y != m)
-            {
-                dpset(state, y, 0);
-                dpset(state, y + 1, p1);
-                dp[now][state] += val;
-            }
+                Set(State, y, 0), Set(State, y + 1, plug1), f[now][State] += Val;
         }
-        else if (!p1 && p2)
+        else if (!plug1 && plug2)
         {
             if (y != m)
-                dp[now][state] += val;
+                f[now][State] += Val;
             if (x != n)
-            {
-                dpset(state, y, p2);
-                dpset(state, y + 1, 0);
-                dp[now][state] += val;
-            }
+                Set(State, y, plug2), Set(State, y + 1, 0), f[now][State] += Val;
         }
-        else if (p1 == 1 && p2 == 1)
-        {
-            dpset(state, link(state, y + 1), 1);
-            dpset(state, y, 0);
-            dpset(state, y + 1, 0);
-            dp[now][state] += val;
-        }
-        else if (p1 == 1 && p2 == 2)
+        else if (plug1 == 1 && plug2 == 1)
+            Set(State, Link(State, y + 1), 1), Set(State, y, 0), Set(State, y + 1, 0), f[now][State] += Val;
+        else if (plug1 == 1 && plug2 == 2)
         {
             if (x == n && y == m)
-                ans += val;
+                Ans += Val;
         }
-        else if (p1 == 2 && p2 == 1)
-        {
-            dpset(state, y, 0);
-            dpset(state, y + 1, 0);
-            dp[now][state] += val;
-        }
-        else if (p1 == 2 && p2 == 2)
-        {
-            dpset(state, link(state, y), 2);
-            dpset(state, y, 0);
-            dpset(state, y + 1, 0);
-            dp[now][state] += val;
-        }
+        else if (plug1 == 2 && plug2 == 1)
+            Set(State, y, 0), Set(State, y + 1, 0), f[now][State] += Val;
+        else if (plug1 == 2 && plug2 == 2)
+            Set(State, Link(State, y), 2), Set(State, y, 0), Set(State, y + 1, 0), f[now][State] += Val;
     }
 }
 int main()
@@ -180,25 +129,24 @@ int main()
     scanf("%d%d", &n, &m);
     if (n == 1 || m == 1)
     {
-        puts("1");
+        printf("1\n");
         return 0;
     }
     if (m > n)
         swap(n, m);
-    dp[0].init();
-    dp[0][0] = 1;
+    f[0].Initialize();
+    f[0][0] = 1;
     for (int i = 1; i <= n; i++)
     {
         for (int j = 1; j <= m; j++)
-            run(i, j);
+            Execution(i, j);
         if (i != n)
         {
-            int now = (i * m) & 1, tot = dp[now].sz;
+            int now = (i * m) & 1, tot = f[now].size;
             for (int j = 1; j <= tot; j++)
-                dp[now].key[j] <<= 2;
+                f[now].key[j] <<= 2;
         }
     }
-    ans += ans;
-    ans.print();
-    return 0;
+    Ans += Ans;
+    Ans.Print();
 }
