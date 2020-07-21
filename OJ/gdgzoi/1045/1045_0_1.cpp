a132a1b6703c01f97e5db7ff7bb2217b906d1541
zhangtianli2006 <49156174+zhangtianli2006@users.noreply.github.com>
2020-07-21 19:44:50 +0800

    7-21: network flow

---
+++ OJ/gdgzoi/1045/1045_0_1.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <string.h>
+#include <vector>
+#include <queue>
+using namespace std;
+const int INF = 0x7fffffff;
+const int MAXN = 210;
+int g[MAXN][MAXN];
+int depth[MAXN];
+int n, m, s, t;
+bool dinic_bfs()
+{
+    memset(depth, 0, sizeof(depth));
+    queue<int> q;
+    q.push(s);
+    while (!q.empty())
+    {
+        int u = q.front();
+        q.pop();
+        for (int i = 1; i <= n; i++)
+        {
+            if (!depth[i] && g[u][i])
+            {
+                depth[i] = depth[u] + 1;
+                q.push(i);
+            }
+        }
+    }
+    if (!depth[t])
+        return false;
+    return true;
+}
+int dinic_dfs(int u, int minn)
+{
+    if (u == t)
+        return minn;
+    for (int i = 1; i <= n; i++)
+    {
+        if (depth[i] == depth[u] + 1 && g[u][i])
+        {
+            int tmp = dinic_dfs(i, min(minn, g[u][i]));
+            if (tmp)
+            {
+                g[u][i] -= tmp;
+                g[i][u] += tmp;
+                return tmp;
+            }
+        }
+    }
+    return 0;
+}
+long long dinic()
+{
+    long long ans = 0;
+    while (dinic_bfs())
+    {
+        int tmp;
+        while (tmp = dinic_dfs(s, INF))
+            ans += tmp;
+    }
+    return ans;
+}
+int main()
+{
+    cin >> m >> n;
+    s = 1;
+    t = n;
+    for (int i = 0; i < m; i++)
+    {
+        int u, v, w;
+        cin >> u >> v >> w;
+        g[u][v] += w;
+    }
+    cout << dinic();
+    return 0;
+}
---
+++ OJ/gdgzoi/2217/2217_0.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <string.h>
+#include <vector>
+#include <queue>
+using namespace std;
+const int INF = 0x7fffffff;
+const int MAXN = 410;
+int s, n, f, d, t;
+int g[MAXN][MAXN];
+int depth[MAXN];
+bool dinic_bfs()
+{
+    memset(depth, 0, sizeof(depth));
+    queue<int> q;
+    q.push(s);
+    while (!q.empty())
+    {
+        int u = q.front();
+        q.pop();
+        for (int i = s; i <= t; i++)
+        {
+            if (!depth[i] && g[u][i])
+            {
+                depth[i] = depth[u] + 1;
+                q.push(i);
+            }
+        }
+    }
+    if (!depth[t])
+        return false;
+    return true;
+}
+int dinic_dfs(int u, int minn)
+{
+    if (u == t)
+        return minn;
+    for (int i = s; i <= t; i++)
+    {
+        if (depth[i] == depth[u] + 1 && g[u][i])
+        {
+            int next = dinic_dfs(i, min(minn, g[u][i]));
+            if (next > 0)
+            {
+                g[i][u] += next;
+                g[u][i] -= next;
+                return next;
+            }
+        }
+    }
+    return 0;
+}
+int dinic()
+{
+    int ans = 0;
+    while (dinic_bfs())
+    {
+        int tmp;
+        while (tmp = dinic_dfs(s, INF))
+            ans++;
+    }
+    return ans;
+}
+int main()
+{
+    cin >> n >> f >> d;
+    s = 0;
+    t = n + f + d + 1;
+    for (int i = 1; i <= n; i++)
+    {
+        int ff, dd;
+        cin >> ff >> dd;
+        for (int j = 0; j < ff; j++)
+        {
+            int fff;
+            cin >> fff;
+            g[s][fff] = 1;
+            g[fff][f + i] = 1;
+        }
+        for (int j = 0; j < dd; j++)
+        {
+            int ddd;
+            cin >> ddd;
+            g[f + n + ddd][t] = 1;
+            g[f + i][f + n + ddd] = 1;
+        }
+    }
+    cout << dinic();
+    return 0;
+}
