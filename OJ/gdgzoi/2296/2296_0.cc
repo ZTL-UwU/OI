31ab3ce8d0f943d3cc3a7cfb59b25c9860d10899
zhangtianli2006 <49156174+zhangtianli2006@users.noreply.github.com>
2020-07-22 14:58:49 +0800

    7-22: manage files & network flow

---
+++ OJ/gdgzoi/2296/2296_0.cc
@@ -0,0 +1,116 @@
+#include <iostream>
+#include <string.h>
+#include <vector>
+#include <queue>
+using namespace std;
+struct data2 { int minn, dis; };
+struct data { int v, w, f; };
+const int INF = 0x7fffffff;
+const int MAXN = 500;
+vector<data> g[MAXN];
+bool inque[MAXN];
+int minn[MAXN];
+int pre[MAXN];
+int dis[MAXN];
+int n, m, s, t;
+data2 EK_bfs()
+{
+    memset(inque, 0, sizeof(inque));
+    memset(minn, 127, sizeof(minn));
+    memset(dis, 127, sizeof(dis));
+    for (int i = 0; i < MAXN; i++) pre[i] = -1;
+    queue<int> q;
+    q.push(s);
+    dis[s] = 0;
+    inque[s] = true;
+    while (!q.empty())
+    {
+        int u = q.front();
+        q.pop();
+        inque[u] = false;
+        for (int i = 0; i < g[u].size(); i++)
+        {
+            int v = g[u][i].v;
+            int w = g[u][i].w;
+            int f = g[u][i].f;
+            if (w > 0 && dis[v] > dis[u] + f)
+            {
+                dis[v] = dis[u] + f;
+                pre[v] = u;
+                minn[v] = min(minn[u], w);
+                if (!inque[v])
+                {
+                    inque[v] = true;
+                    q.push(v);
+                }
+            }
+        }
+    }
+    data2 ret;
+    if (pre[t] == -1)
+        ret.minn = -1;
+    else
+    {
+        ret.minn = minn[t];
+        ret.dis = dis[t];
+    }
+    return ret;
+}
+long long ans1, ans2;
+void EK()
+{
+    while (1)
+    {
+        data2 tmp = EK_bfs();
+        if (tmp.minn == -1)
+            break;
+        int u = t;
+        while (u != s)
+        {
+            int tmp_f;
+            for (int i = 0; i < g[pre[u]].size(); i++)
+            {
+                if (g[pre[u]][i].v == u)
+                {
+                    g[pre[u]][i].w -= tmp.minn;
+                    tmp_f = g[pre[u]][i].f;
+                }
+            }
+            bool flag = true;
+            for (int i = 0; i < g[u].size(); i++)
+            {
+                if (g[u][i].v == pre[u])
+                {
+                    g[u][i].w += tmp.minn;
+                    flag = false;
+                }
+            }
+            if (flag)
+                g[u].push_back((data){pre[u], tmp.minn, -tmp_f});
+            u = pre[u];
+        }
+        ans1 += tmp.minn;
+        ans2 += tmp.minn * tmp.dis;
+    }
+}
+int main()
+{
+    cin >> n >> m;
+    s = 1;
+    t = n;
+    for (int i = 0; i < m; i++)
+    {
+        int u, v, f;
+        cin >> u >> v >> f;
+        bool flag = true;
+        for (int i = 0; i < g[u].size(); i++)
+            if (g[u][i].v == u + n)
+                flag = false;
+        if (flag)
+            g[u].push_back((data){u + n, 1, 0});
+        g[u + n].push_back((data){v, INF, f});
+    }
+    EK();
+    cout << ans1 << " " << ans2;
+    return 0;
+}
