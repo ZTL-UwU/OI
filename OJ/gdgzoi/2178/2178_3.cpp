92e48658d9cfb1da13b8af257030390b4142919c
zhangtianli2006 <49156174+zhangtianli2006@users.noreply.github.com>
2020-07-25 09:42:59 +0800

    7-25: AC 2178_3
    Signed-off-by: zhangtianli2006 <49156174+zhangtianli2006@users.noreply.github.com>

--- OJ/gdgzoi/2178/2178_3.cpp
+++ OJ/gdgzoi/2178/2178_3.cpp
@@ -1,37 +1,77 @@
 #include <iostream>
+#include <string.h>
 #include <stdio.h>
 #include <vector>
 #include <queue>
 using namespace std;
 struct data { int v, w; };
 const int INF = 0x7fffffff;
-const int MAXN = 2000;
+const int MAXN = 2e3;
 vector<data> g[MAXN];
-deque<int> q;
 bool inque[MAXN];
+bool vis[MAXN];
 int dis[MAXN];
-void spfa(int start)
+int cnt[MAXN];
+int n, m;
+inline bool SPFA(int s)
 {
-    inque[start] = true;
-    q.push_back(start);
+    for (int i = 0; i < MAXN; i++) dis[i] = INF;
+    queue<int> q;
+    q.push(s);
+    inque[s] = true;
+    dis[s] = 0;
     while (!q.empty())
     {
         int u = q.front();
-        q.pop_front();
+        q.pop();
         inque[u] = false;
+        vis[u] = true;
+        for (int i = 0; i < g[u].size(); i++)
+        {
+            int v = g[u][i].v;
+            int w = g[u][i].w;
+            if (dis[v] > dis[u] + w)
+            {
+                dis[v] = dis[u] + w;
+                cnt[v] = cnt[u] + 1;
+                if (cnt[v] > n)
+                    return false;
+                if (!inque[v])
+                {
+                    inque[v] = true;
+                    q.push(v);
+                }
+            }
+        }
     }
+    return true;
 }
 int main()
 {
-    for (int i = 0; i < MAXN; i ++) dis[i] = INF;
-    int n, m, start;
-    cin >> n >> m >> start;
-    for (int i = 0; i < m; i ++)
+    int s;
+    cin >> n >> m >> s;
+    for (int i = 0; i < m; i++)
     {
         int u, v, w;
         cin >> u >> v >> w;
         g[u].push_back((data){v, w});
     }
-    spfa(start);
+    bool flag = true;
+    for (int i = 1; i <= n; i++)
+        if (!vis[i])
+            flag &= SPFA(i);
+    if (!flag)
+        puts("-1");
+    else
+    {
+        SPFA(s);
+        for (int i = 1; i <= n; i++)
+        {
+            if (dis[i] == INF)
+                puts("NoPath");
+            else
+                cout << dis[i] << "\n";
+        }
+    }
     return 0;
 }
