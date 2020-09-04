a75273eec0fbc5129c62db152bf24ebe0307cf89
zhangtianli2006 <49156174+zhangtianli2006@users.noreply.github.com>
2020-09-04 18:15:54 +0800

    9-4: ac 1018_0
    Signed-off-by: zhangtianli2006 <49156174+zhangtianli2006@users.noreply.github.com>

---
+++ OJ/gdgzoi/primary/1018/1018_0.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+const int MAXN = 1e6 + 10;
+int a[MAXN];
+int main()
+{
+    int n;
+    std::cin >> n;
+
+    for (int i = 0; i < n; i++)
+        std::cin >> a[i];
+    
+    long long ans = 0;
+    for (int i = 1; i < n; i++)
+        ans += std::max(a[i], a[i - 1]);
+
+    std::cout << ans;
+    return 0;
+}
