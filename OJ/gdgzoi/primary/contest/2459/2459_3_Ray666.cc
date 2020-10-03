cec2e8433440648efe9cc7170c161d1edfec11f8
zhangtianli2006 <zhangtianli2006@163.com>
2020-10-03 08:16:56 +0800

    10-3

--- OJ/gdgzoi/primary/contest/2459/2459_3_Ray666.cc
+++ OJ/gdgzoi/primary/contest/2459/2459_3_Ray666.cc
@@ -6,43 +6,52 @@
 
 using namespace std;
 
-map<int, int> head;
+map<int, int> id;
 
 struct node
 {
-    int v;
+    int state;
     int next;
 } a[4000001];
 
 int f[201], m[201], tot = 0, n, ans = 0;
 bool check[20000001];
 
+// x: strength
+// y: state
+// tot: id -> head
 void add(int x, int y)
 {
-    a[++tot].v = y;
-    a[tot].next = head[x];
-    head[x] = tot;
+    a[++tot].state = y;  // New node y
+    a[tot].next = id[x]; // y.next = x.id
+    id[x] = tot;         // x.id = y.id (tot)
 }
 
+// x: counter
+// y: power of group A
+// z: bin state
 void dfs(int x, int y, int z)
 {
-    if (x > n / 2 - 2)
+    if (x > n / 2 - 2) // Done choosing
     {
         if (y >= 0)
             add(y, z);
         return;
     }
-    dfs(x + 1, y + m[x], z + f[x]);
-    dfs(x + 1, y - m[x], z + f[x]);
-    dfs(x + 1, y, z);
+    dfs(x + 1, y + m[x], z + f[x]); // Choose to group A
+    dfs(x + 1, y - m[x], z + f[x]); // Choose to group B
+    dfs(x + 1, y, z);               // Choose nothing
 }
 
 void work(int x, int y, int z)
 {
-    for (int i = head[y]; i; i = a[i].next)
+    for (int i = id[y]; i; i = a[i].next)
     {
-        if (!check[a[i].v + z])
-            check[a[i].v + z] = true, ans++;
+        if (!check[a[i].state + z])
+        {
+            check[a[i].state + z] = true;
+            ans++;
+        }
     }
     if (x > n)
         return;
@@ -61,8 +70,8 @@ int main()
     for (int i = 2; i <= n; i++)
         f[i] = f[i - 1] * 2;
 
-    dfs(1, 0, 0);
-    work(n / 2 - 1, 0, 0);
+    dfs(1, 0, 0);          // Left half
+    work(n / 2 - 1, 0, 0); // Right half
 
     ans--;
     printf("%d", ans);
---
+++ OJ/gdgzoi/primary/contest/2459/2459_correct.cc
@@ -0,0 +1,11 @@
+#include <algorithm>
+#include <iostream>
+#include <cstring>
+#include <cstdio>
+#include <vector>
+
+int main()
+{
+    
+    return 0;
+}
