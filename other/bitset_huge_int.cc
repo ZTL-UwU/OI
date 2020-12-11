f7bd722f777c851342759e6a2bfcc3f3b483fce6
zhangtianli2006 <zhangtianli2006@163.com>
2020-12-11 21:02:00 +0800

    12-11

--- other/bitset_huge_int.cc
+++ other/bitset_huge_int.cc
@@ -1,4 +1,3 @@
-#include <bits/types/error_t.h>
 #include <stdexcept>
 #include <iostream>
 #include <sstream>
@@ -15,39 +14,40 @@ public:
     huge_int(const T &src);
 
     void operator=(const std::bitset<Size> &src);
-    void operator=(const std::string &src);
-    void operator=(const int &src);
-    void operator=(const unsigned int &src);
-    void operator=(const long long int &src);
-    void operator=(const unsigned long long int &src);
 
     huge_int<Size> operator<<(const size_t &position) const;
     void operator<<=(const size_t &position);
+    huge_int<Size> operator>>(const size_t &position) const;
+    void operator>>=(const size_t &position);
 
-    bool operator==(const huge_int<Size> &huge_int_num) const;
-    bool operator!=(const huge_int<Size> &huge_int_num) const;
+    bool operator==(const huge_int<Size> &val) const;
+    bool operator!=(const huge_int<Size> &val) const;
 
-    bool operator<(const huge_int<Size> &huge_int_num) const;
-    bool operator>(const huge_int<Size> &huge_int_num) const;
+    bool operator<(const huge_int<Size> &val) const;
+    bool operator>(const huge_int<Size> &val) const;
 
-    bool operator<=(const huge_int<Size> &huge_int_num) const;
-    bool operator>=(const huge_int<Size> &huge_int_num) const;
+    bool operator<=(const huge_int<Size> &val) const;
+    bool operator>=(const huge_int<Size> &val) const;
 
-    huge_int<Size> operator+(const huge_int<Size> &huge_int_num) const;
-    void operator+=(const huge_int<Size> &huge_int_num);
+    huge_int<Size> operator+(const huge_int<Size> &val) const;
+    void operator+=(const huge_int<Size> &val);
     huge_int<Size> operator+(const unsigned int &num) const;
     void operator+=(const unsigned int &num);
     huge_int<Size> operator+(const unsigned long long int &num) const;
     void operator+=(const unsigned long long int &num);
 
-    huge_int<Size> operator-(const huge_int<Size> &huge_int_num) const;
-    void operator-=(const huge_int<Size> &huge_int_num);
+    huge_int<Size> operator-(const huge_int<Size> &val) const;
+    void operator-=(const huge_int<Size> &val);
+    huge_int<Size> operator-(const unsigned int &num) const;
+    void operator-=(const unsigned int &num);
+    huge_int<Size> operator-(const unsigned long long int &num) const;
+    void operator-=(const unsigned long long int &num);
 
-    huge_int<Size> operator*(huge_int<Size> huge_int_num) const;
-    void operator*=(const huge_int<Size> &huge_int_num);
+    huge_int<Size> operator*(huge_int<Size> val) const;
+    void operator*=(const huge_int<Size> &val);
 
-    huge_int<Size> operator/(const huge_int<Size> &huge_int_num) const;
-    void operator/=(const huge_int<Size> &huge_int_num);
+    huge_int<Size> operator/(const huge_int<Size> &val) const;
+    void operator/=(const huge_int<Size> &val);
 
     std::string to_string() const;
     unsigned int to_uint() const;
@@ -56,144 +56,122 @@ public:
     std::bitset<Size> to_bitset() const;
 
 private:
-    std::bitset<Size> huge_int_base;
+    std::bitset<Size> _base;
 };
 
 template <size_t Size>
 huge_int<Size>::huge_int()
 {
-    this->huge_int_base = std::bitset<Size>(0);
+    this->_base = std::bitset<Size>(0);
 }
 
 template <size_t Size>
 template <typename T>
 huge_int<Size>::huge_int(const T &src)
 {
-    this->huge_int_base = std::bitset<Size>(src);
+    this->_base = std::bitset<Size>(src);
 }
 
 template <size_t Size>
 void huge_int<Size>::operator=(const std::bitset<Size> &src)
 {
-    this->huge_int_base = src;
-}
-
-template <size_t Size>
-void huge_int<Size>::operator=(const std::string &src)
-{
-    huge_int res;
-    for (auto ch : src)
-        res = huge_int(res.huge_int_base << 3) + huge_int(res.huge_int_base << 1) + huge_int(ch - '0');
-
-    this->huge_int_base = res.huge_int_base;
-}
-
-template <size_t Size>
-void huge_int<Size>::operator=(const int &src)
-{
-    *this = std::to_string(src);
-}
-
-template <size_t Size>
-void huge_int<Size>::operator=(const unsigned int &src)
-{
-    *this = std::to_string(src);
-}
-
-template <size_t Size>
-void huge_int<Size>::operator=(const long long int &src)
-{
-    *this = std::to_string(src);
-}
-
-template <size_t Size>
-void huge_int<Size>::operator=(const unsigned long long int &src)
-{
-    *this = std::to_string(src);
+    this->_base = src;
 }
 
 template <size_t Size>
 huge_int<Size> huge_int<Size>::operator<<(const size_t &position) const
 {
-    return this->huge_int_base << position;
+    return this->_base << position;
 }
 
 template <size_t Size>
 void huge_int<Size>::operator<<=(const size_t &position)
 {
-    this->huge_int_base <<= position;
+    this->_base <<= position;
 }
 
 template <size_t Size>
-bool huge_int<Size>::operator==(const huge_int<Size> &huge_int_num) const
+huge_int<Size> huge_int<Size>::operator>>(const size_t &position) const
 {
-    for (auto i = this->huge_int_base.size() - 1; i >= 0; i--)
-        if (this->huge_int_base[i] != huge_int_num.huge_int_base[i])
+    return this->_base >> position;
+}
+
+template <size_t Size>
+void huge_int<Size>::operator>>=(const size_t &position)
+{
+    this->_base >>= position;
+}
+
+template <size_t Size>
+bool huge_int<Size>::operator==(const huge_int<Size> &val) const
+{
+    for (auto i = Size - 1; i <= Size - 1; i--)
+        if (this->_base[i] != val._base[i])
             return false;
 
     return true;
 }
 
 template <size_t Size>
-bool huge_int<Size>::operator!=(const huge_int<Size> &huge_int_num) const
+bool huge_int<Size>::operator!=(const huge_int<Size> &val) const
 {
-    if (this == huge_int_num)
+    if (this == val)
         return false;
 
     return true;
 }
 
 template <size_t Size>
-bool huge_int<Size>::operator<(const huge_int<Size> &huge_int_num) const
+bool huge_int<Size>::operator<(const huge_int<Size> &val) const
 {
-    for (auto i = this->huge_int_base.size() - 1; i >= 0; i--)
-        if (this->huge_int_base[i] != huge_int_num.huge_int_base[i])
-            return this->huge_int_base[i] < huge_int_num.huge_int_base[i];
+    for (auto i = Size - 1; i <= Size - 1; i--)
+        if (this->_base[i] != val._base[i])
+            return this->_base[i] < val._base[i];
 
     return false;
 }
 
 template <size_t Size>
-bool huge_int<Size>::operator>(const huge_int<Size> &huge_int_num) const
+bool huge_int<Size>::operator>(const huge_int<Size> &val) const
 {
-    if (this < huge_int_num or this == huge_int_num)
+    if (*this < val or *this == val)
         return false;
 
     return true;
 }
 
 template <size_t Size>
-bool huge_int<Size>::operator<=(const huge_int<Size> &huge_int_num) const
+bool huge_int<Size>::operator<=(const huge_int<Size> &val) const
 {
-    if (this < huge_int_num or this == huge_int_num)
+    if (*this < val or *this == val)
         return true;
 
     return false;
 }
 
 template <size_t Size>
-bool huge_int<Size>::operator>=(const huge_int<Size> &huge_int_num) const
+bool huge_int<Size>::operator>=(const huge_int<Size> &val) const
 {
-    if (this > huge_int_num or this == huge_int_num)
+    if (*this > val or *this == val)
         return true;
 
     return false;
 }
 
 template <size_t Size>
-huge_int<Size> huge_int<Size>::operator+(const huge_int<Size> &huge_int_num) const
+huge_int<Size> huge_int<Size>::operator+(const huge_int<Size> &val) const
 {
-    huge_int a(this->huge_int_base ^ huge_int_num.huge_int_base);
-    huge_int b((this->huge_int_base & huge_int_num.huge_int_base) << 1);
+    huge_int a(this->_base ^ val._base);
+    huge_int b((this->_base & val._base) << 1);
 
-    return huge_int_num.huge_int_base.any() ? a + b : a;
+    return val._base.any() ? a + b : a;
 }
 
 template <size_t Size>
-void huge_int<Size>::operator+=(const huge_int<Size> &huge_int_num)
+void huge_int<Size>::operator+=(const huge_int<Size> &val)
 {
-    huge_int<Size> res = (*this + huge_int_num);
-    this->huge_int_base = res.huge_int_base;
+    huge_int<Size> res = (*this + val);
+    this->_base = res._base;
 }
 
 template <size_t Size>
@@ -205,51 +183,74 @@ huge_int<Size> huge_int<Size>::operator+(const unsigned int &num) const
 template <size_t Size>
 void huge_int<Size>::operator+=(const unsigned int &num)
 {
-    huge_int<Size> res = (*this + huge_int_num);
-    this->huge_int_base = res.huge_int_base;
+    huge_int<Size> res = (*this + num);
+    this->_base = res._base;
 }
 
 template <size_t Size>
 huge_int<Size> huge_int<Size>::operator+(const unsigned long long int &num) const
 {
-    huge_int a(this->huge_int_base ^ huge_int_num.huge_int_base);
-    huge_int b((this->huge_int_base & huge_int_num.huge_int_base) << 1);
-
-    return huge_int_num.huge_int_base.any() ? a + b : a;
+    return *this + huge_int(num);
 }
 
 template <size_t Size>
 void huge_int<Size>::operator+=(const unsigned long long int &num)
 {
-    huge_int<Size> res = (*this + huge_int_num);
-    this->huge_int_base = res.huge_int_base;
+    huge_int<Size> res = (*this + num);
+    this->_base = res._base;
 }
 
 template <size_t Size>
-huge_int<Size> huge_int<Size>::operator-(const huge_int<Size> &huge_int_num) const
+huge_int<Size> huge_int<Size>::operator-(const huge_int<Size> &val) const
 {
-    return *this + (huge_int(std::bitset<Size>(1)) + huge_int(~huge_int_num.huge_int_base));
+    return *this + (huge_int(std::bitset<Size>(1)) + huge_int(~val._base));
 }
 
 template <size_t Size>
-void huge_int<Size>::operator-=(const huge_int<Size> &huge_int_num)
+void huge_int<Size>::operator-=(const huge_int<Size> &val)
 {
-    huge_int<Size> res = *this - huge_int_num;
-    this->huge_int_base = res.huge_int_base;
+    huge_int<Size> res = *this - val;
+    this->_base = res._base;
 }
 
 template <size_t Size>
-huge_int<Size> huge_int<Size>::operator*(huge_int<Size> huge_int_num) const
+huge_int<Size> huge_int<Size>::operator-(const unsigned int &num) const
+{
+    return *this - huge_int(num);
+}
+
+template <size_t Size>
+void huge_int<Size>::operator-=(const unsigned int &num)
+{
+    huge_int<Size> res = *this - num;
+    this->_base = res._base;
+}
+
+template <size_t Size>
+huge_int<Size> huge_int<Size>::operator-(const unsigned long long int &num) const
+{
+    return *this - huge_int(num);
+}
+
+template <size_t Size>
+void huge_int<Size>::operator-=(const unsigned long long int &num)
+{
+    huge_int<Size> res = *this - num;
+    this->_base = res._base;
+}
+
+template <size_t Size>
+huge_int<Size> huge_int<Size>::operator*(huge_int<Size> val) const
 {
     huge_int tmp_this;
     tmp_this = *this;
 
     huge_int res(std::bitset<Size>(0));
-    while (huge_int_num.huge_int_base.any())
+    while (val._base.any())
     {
-        if (huge_int_num.huge_int_base[0])
+        if (val._base[0])
             res += tmp_this;
-        huge_int_num.huge_int_base >>= 1;
+        val._base >>= 1;
         tmp_this += tmp_this;
     }
 
@@ -257,17 +258,29 @@ huge_int<Size> huge_int<Size>::operator*(huge_int<Size> huge_int_num) const
 }
 
 template <size_t Size>
-huge_int<Size> huge_int<Size>::operator/(const huge_int<Size> &huge_int_num) const
+huge_int<Size> huge_int<Size>::operator/(const huge_int<Size> &val) const
 {
     huge_int<Size> tmp = *this;
     huge_int<Size> res;
 
-    while (tmp > huge_int_num)
+    while (tmp > val)
     {
         int k = 0;
-        for (huge_int c = tmp; tmp >= c; c <<= 1, k++)
-            if (tmp < huge_int_num + c)
-                res += 1 << k;
+        huge_int c;
+
+        for (c = tmp; tmp >= c; c <<= 1, k++)
+        {
+            if (tmp < val + c)
+            {
+                res += (unsigned int)1 << k;
+                break;
+            }
+        }
+
+        if (tmp < val + c)
+            break;
+        res += (unsigned int)1 << (k - 1);
+        tmp -= c >> 1;
     }
 
     return res;
@@ -280,9 +293,9 @@ std::string huge_int<Size>::to_string() const
     std::vector<unsigned char> res_v;
     res_v.push_back(0);
 
-    for (auto i = this->huge_int_base.size() - 1; i <= this->huge_int_base.size() - 1; i--)
+    for (auto i = this->_base.size() - 1; i <= this->_base.size() - 1; i--)
     {
-        int front = this->huge_int_base.test(i);
+        int front = this->_base.test(i);
 
         for (auto &col : res_v)
         {
@@ -320,7 +333,7 @@ unsigned int huge_int<Size>::to_uint() const
     if (Size > 32)
         throw std::overflow_error("huge_int size (" + std::to_string(Size) + ") out of unsigned int (32)");
 
-    return this->huge_int_base.to_ulong();
+    return this->_base.to_ulong();
 }
 
 template <size_t Size>
@@ -329,19 +342,19 @@ unsigned long long int huge_int<Size>::to_ullong() const
     if (Size > 64)
         throw std::overflow_error("huge_int size (" + std::to_string(Size) + ") out of unsigned int (64)");
 
-    return this->huge_int_base.to_ullong();
+    return this->_base.to_ullong();
 }
 
 template <size_t Size>
 std::string huge_int<Size>::to_bin_string() const
 {
-    return this->huge_int_base.to_string();
+    return this->_base.to_string();
 }
 
 template <size_t Size>
 std::bitset<Size> huge_int<Size>::to_bitset() const
 {
-    return this->huge_int_base;
+    return this->_base;
 }
 
 int main()
@@ -349,11 +362,9 @@ int main()
     huge_int<1024> a;
     huge_int<1024> b;
 
-    a = "11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111"
-        "1111111111111";
-    b = "11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111"
-        "1111111111100";
+    a = std::string("10");
+    b = std::string("2");
 
-    std::cout << (a - b).to_string();
+    std::cout << (a == 2);
     return 0;
 }
