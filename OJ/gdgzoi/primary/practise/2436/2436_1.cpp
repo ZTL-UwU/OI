5e0eb8ed80287a7bacfc97e9caa119e38958a318
zhangtianli2006 <zhangtianli2006@163.com>
2021-01-27 11:08:14 +0800

    1-27

--- OJ/gdgzoi/primary/practise/2436/2436_1.cpp
+++ OJ/gdgzoi/primary/practise/2436/2436_1.cpp
@@ -4,25 +4,29 @@
 using namespace std;
 const int INF = 0x7fffffff;
 int prime[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31};
-unsigned long long ans = 0;
-unsigned long long maxn_num = -INF;
+unsigned long long ans = INF;
+unsigned long long maxn_num;
 int n;
 void dfs(int k, int last, unsigned long long fac, unsigned long long num)
 {
-    if (fac > n) return;
-    if (num > maxn_num) maxn_num = num;
+    if (fac > n)
+        return;
     if (k == 10)
     {
-        if (num == maxn_num) ans = min(ans, fac);
-        if (num > maxn_num) ans = fac;
+        if (num == maxn_num)
+            ans = min(ans, fac);
+        if (num > maxn_num)
+            ans = fac, maxn_num = num;
         return;
     }
-    for (int i = 0; i <= i; i ++) dfs(k + 1, i, fac *= prime[k], num * (i + 1));
+    int tmp = 1;
+    for (int i = 0; i <= last; i++)
+        dfs(k + 1, i, fac * tmp, num * (i + 1)), tmp *= prime[k];
 }
 int main()
 {
     cin >> n;
     dfs(0, 31, 1, 1);
-    cout << ans;
+    cout << ans << "\n";
     return 0;
 }
