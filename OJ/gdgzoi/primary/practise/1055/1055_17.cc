870cff95ba168278bb7e733ee569088fd3ea78e4
zhangtianli2006 <zhangtianli2006@163.com>
2021-05-23 12:46:58 +0800

    [5-23]: ac 1055_17
    Signed-off-by: zhangtianli2006 <zhangtianli2006@163.com>

--- OJ/gdgzoi/primary/practise/1055/1055_17.cc
+++ OJ/gdgzoi/primary/practise/1055/1055_17.cc
@@ -1,38 +1,42 @@
+#include <cstddef>
 #include <iostream>
-#include <string.h>
-#include <stdio.h>
 #include <vector>
-#include <math.h>
-using namespace std;
-const int MAXN = 2e6 + 2;
-vector<int> v[5371];
+#include <cmath>
+
+const int MAXN = 2000010;
+std::vector<int> v[6000];
 int rmq[MAXN][22];
+
 int main()
 {
     int n, k, p;
-    scanf("%d %d %d", &n, &k, &p);
+    std::cin >> n >> k >> p;
+
     for (int i = 1; i <= n; i ++)
     {
         int col, money;
-        scanf("%d %d", &col, &money);
+        std::cin >> col >> money;
+
         v[col].push_back(i);
         rmq[i][0] = money;
     }
-    int tmp = log2(n);
+
+    const int tmp = std::log2(n);
     for (int i = 1; i < tmp; i ++)
         for (int j = 1; j + (1 << i) - 1 <= n; j ++)
-            rmq[j][i] = min(rmq[j][i - 1], rmq[j + (1 << (i - 1))][i - 1]);
-    int ans = 0;
+            rmq[j][i] = std::min(rmq[j][i - 1], rmq[j + (1 << (i - 1))][i - 1]);
+
+    long long int ans = 0;
     for (int i = 0; i < k; i ++)
     {
-        for (int j = 0; j < v[i].size() - 1; j ++)
+        for (std::size_t j = 0; j < v[i].size() - 1; j ++)
         {
-            int l = v[i][j];
-            for (int x = j + 1; x < v[i].size(); x ++)
+            const int l = v[i][j];
+            for (std::size_t x = j + 1; x < v[i].size(); x ++)
             {
-                int r = v[i][x];
-                int k = log2(r - l + 1);
-                int res = min(rmq[l][k], rmq[r - (1 << k) + 1][k]);
+                const int r = v[i][x];
+                const int kk = std::log2(r - l + 1);
+                const int res = std::min(rmq[l][kk], rmq[r - (1 << kk) + 1][kk]);
                 if (res <= p)
                 {
                     ans += v[i].size() - x;
@@ -41,6 +45,7 @@ int main()
             }
         }
     }
-    printf("%d\n", ans);
+
+    std::cout << ans;
     return 0;
 }
