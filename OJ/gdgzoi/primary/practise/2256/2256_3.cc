3caec161bdf869bc7a75b12d56439533e5e52a2e
zhangtianli2006 <zhangtianli2006@163.com>
2021-07-13 11:17:09 +0800

    [7-13]
    Signed-off-by: zhangtianli2006 <zhangtianli2006@163.com>

--- OJ/gdgzoi/primary/practise/2256/2256_3.cc
+++ OJ/gdgzoi/primary/practise/2256/2256_3.cc
@@ -2,18 +2,20 @@
 
 #include <iostream>
 #include <climits>
+#include <cassert>
+#include <memory>
 
 const int INF = INT_MAX;
 
 struct node
 {
     bool is_zero = true;
-    node *l_son;
-    node *r_son;
+    std::shared_ptr<node> l_son;
+    std::shared_ptr<node> r_son;
     int id;
 
     node() {}
-    node(bool is_zero, node *l_son, node *r_son, int id)
+    node(bool is_zero, std::shared_ptr<node> l_son, std::shared_ptr<node> r_son, int id)
     {
         this->is_zero = is_zero;
         this->l_son = l_son;
@@ -21,15 +23,25 @@ struct node
         this->id = id;
     }
 
-    int get_son_size(node *son)
+    int get_son_size(std::shared_ptr<node> son)
     {
         if (is_zero)
             return 1;
-        return son->get_son_size(son->l_son) + son->get_son_size(son->r_son) + 1;
+        if (son)
+            return son->get_son_size(son->l_son) + son->get_son_size(son->r_son) + 1;
     }
 };
 
-node *max(node *p1, node *p2)
+int get_size(std::shared_ptr<node> p)
+{
+    if (p->is_zero)
+        return 1;
+    assert(p->l_son != nullptr);
+    assert(p->r_son != nullptr);
+    return get_size(p->l_son) + get_size(p->r_son) + 1;
+}
+
+std::shared_ptr<node> max(std::shared_ptr<node> p1, std::shared_ptr<node> p2)
 {
     if (!p1)
         return p2;
@@ -43,16 +55,16 @@ node *max(node *p1, node *p2)
     if (p2->is_zero)
         return p1;
 
-    const int p1_l = p1->get_son_size(p1->l_son);
-    const int p2_l = p2->get_son_size(p2->l_son);
+    const int p1_l = get_size(p1->l_son);
+    const int p2_l = get_size(p2->l_son);
 
     if (p1_l > p2_l)
         return p1;
     if (p1_l < p2_l)
         return p2;
 
-    const int p1_r = p1->get_son_size(p1->r_son);
-    const int p2_r = p2->get_son_size(p2->r_son);
+    const int p1_r = get_size(p1->r_son);
+    const int p2_r = get_size(p2->r_son);
 
     return p1_r > p2_r ? p1 : p2;
 }
@@ -64,7 +76,7 @@ int n, m;
 
 namespace ST
 {
-    node *tree[4 * MAX_N];
+    std::shared_ptr<node> tree[4 * MAX_N];
 
     void push_up(int k)
     {
@@ -88,7 +100,7 @@ namespace ST
         return find(tar, mid + 1, r, rs);
     }
 
-    void update(int tar, node *new_tree, int l = 1, int r = n, int k = 1)
+    void update(int tar, std::shared_ptr<node> new_tree, int l = 1, int r = n, int k = 1)
     {
         if (l == tar && r == tar)
         {
@@ -107,7 +119,7 @@ namespace ST
         push_up(k);
     }
 
-    node *query(int l_tar, int r_tar, int l = 1, int r = n, int k = 1)
+    std::shared_ptr<node> query(int l_tar, int r_tar, int l = 1, int r = n, int k = 1)
     {
         if (l_tar <= l && r <= r_tar)
             return tree[k];
@@ -116,7 +128,7 @@ namespace ST
         const int rs = k * 2 + 1;
         const int mid = (l + r) / 2;
 
-        node *res;
+        std::shared_ptr<node> res;
         if (l_tar <= mid)
             res = query(l_tar, r_tar, l, mid, ls);
         if (r_tar > mid)
@@ -135,9 +147,8 @@ int main()
 
     const int bottom_layer_start = ST::find(1) - 1;
     for (int i = 1; i <= n; i++)
-        ST::tree[bottom_layer_start + i] = new node(true, nullptr, nullptr, i);
+        ST::tree[bottom_layer_start + i] = std::make_shared<node>(true, nullptr, nullptr, i);
 
-    node new_tree;
     for (int _i = 0; _i < m; _i++)
     {
         char op;
@@ -148,12 +159,9 @@ int main()
             int l, r, k;
             std::cin >> l >> r >> k;
 
-            new_tree.is_zero = false;
-            new_tree.l_son = ST::tree[bottom_layer_start + l];
-            new_tree.r_son = ST::tree[bottom_layer_start + r];
-            new_tree.id = k;
-
-            ST::update(k, &new_tree);
+            auto new_tree =
+                std::make_shared<node>(false, ST::tree[bottom_layer_start + l], ST::tree[bottom_layer_start + r], k);
+            ST::update(k, new_tree);
         }
         else if (op == 'Q')
         {
