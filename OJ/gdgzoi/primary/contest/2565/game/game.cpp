9bdd8b16d153f604832bff2389746ae624971616
zhangtianli2006 <zhangtianli2006@163.com>
2021-10-05 13:14:10 +0800

    [10-5] ac 2565 game
    Signed-off-by: zhangtianli2006 <zhangtianli2006@163.com>

--- OJ/gdgzoi/primary/contest/2565/game/game.cpp
+++ OJ/gdgzoi/primary/contest/2565/game/game.cpp
@@ -1,17 +1,17 @@
+#include <unordered_map>
 #include <iostream>
 #include <cstring>
-#include <cstdio>
 #include <vector>
-#include <map>
-using namespace std;
 
 const int MAX_N = 4e3 + 50;
 
-map<string, int> id;
+using pii = std::pair<int, int>;
+
+std::unordered_map<std::string, int> id;
 int id_cnt;
 
-vector<int> g[MAX_N];
-vector<int> nums;
+std::vector<int> g[MAX_N];
+std::vector<int> nums;
 int tot;
 
 int color[MAX_N];
@@ -27,22 +27,20 @@ void init()
     memset(color, 0, sizeof(color));
 }
 
-pair<int, int> dfs(int u)
+pii dfs(int u)
 {
-    pair<int, int> res(0, 0);
+    pii res{0, 0};
     if (color[u] == -1)
         res.first = 1;
     else
         res.second = 1;
 
-    for (size_t i = 0; i < g[u].size(); i++)
+    for (auto v : g[u])
     {
-        const int v = g[u][i];
-
         if (color[v] == 0)
         {
             color[v] = (color[u] == 1 ? -1 : 1);
-            const pair<int, int> ret = dfs(v);
+            const auto ret = dfs(v);
             res.first += ret.first;
             res.second += ret.second;
         }
@@ -51,18 +49,14 @@ pair<int, int> dfs(int u)
     return res;
 }
 
-bool dfs2(size_t step, size_t last, int sum, const size_t size, const bool diff)
+bool dfs2(std::size_t step, std::size_t last, int sum, const std::size_t size)
 {
     if (step == size / 2)
-    {
-        if (diff)
-            return (2 * sum - 1 == tot || 2 * sum + 1 == tot);
         return 2 * sum == tot;
-    }
 
-    for (size_t i = last + 1; i < size; i++)
+    for (std::size_t i = last + 1; i < size; i++)
     {
-        const bool ret = dfs2(step + 1, i, sum + nums[i], size, diff);
+        const bool ret = dfs2(step + 1, i, sum + nums[i], size);
         if (ret)
             return true;
     }
@@ -77,20 +71,19 @@ int main()
     std::cin.tie(0);
 
     int t;
-    cin >> t;
+    std::cin >> t;
 
     for (int _i = 0; _i < t; _i++)
     {
         int n, m;
-        cin >> n >> m;
+        std::cin >> n >> m;
 
         init();
 
-        bool flag = true;
         for (int _i = 0; _i < m; _i++)
         {
-            string u, v;
-            cin >> u >> v;
+            std::string u, v;
+            std::cin >> u >> v;
 
             if (!id[u])
             {
@@ -103,13 +96,13 @@ int main()
                 id[v] = id_cnt;
             }
 
-            g[id[u]].push_back(id[v]);
-            g[id[v]].push_back(id[u]);
+            g[id[u]].emplace_back(id[v]);
+            g[id[v]].emplace_back(id[u]);
         }
 
         if (2 * n - id_cnt >= 2)
         {
-            cout << "NO\n";
+            std::cout << "NO\n";
             continue;
         }
 
@@ -118,15 +111,15 @@ int main()
             if (color[i] == 0)
             {
                 color[i] = 1;
-                const pair<int, int> ret = dfs(i);
+                const auto ret = dfs(i);
                 tot += ret.first + ret.second;
-                nums.push_back(ret.first);
-                nums.push_back(ret.second);
+                nums.emplace_back(ret.first);
+                nums.emplace_back(ret.second);
             }
         }
 
-        const bool ret = dfs2(0, -1, 0, nums.size(), 2 * n - id_cnt == 1 ? true : false);
-        cout << (ret ? "YES" : "NO") << "\n";
+        const bool ret = dfs2(0, -1, 0, nums.size());
+        std::cout << (ret ? "YES" : "NO") << "\n";
     }
 
     return 0;
