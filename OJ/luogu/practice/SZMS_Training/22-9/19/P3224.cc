3870d954ccb09be64efbc6e1c85149585267c843
zhangtianli2006 <zhangtianli2006@163.com>
2022-09-19 09:16:29 +0800

    [22-9-19]
    Signed-off-by: zhangtianli2006 <zhangtianli2006@163.com>

---
+++ OJ/luogu/practice/SZMS_Training/22-9/19/P3224.cc
@@ -0,0 +1,99 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+const int MAX_N = 1e5 + 10;
+int importance[MAX_N];
+
+template <typename T>
+class vvt
+{
+public:
+    std::vector<T> base;
+
+    vvt() {}
+    vvt(std::size_t size) { this->base.reserve(size); }
+    ~vvt() { delete &this->base; }
+
+    T operator[](std::size_t rank) { return this->base[rank - 1]; }
+    void insert(T val) { this->base.insert(std::lower_bound(this->base.begin(), this->base.end(), val), val); }
+    void merge(vvt<T> &x)
+    {
+        for (auto i : x.base)
+            this->insert(i);
+    }
+};
+
+vvt<int> t[MAX_N];
+
+class ufind
+{
+private:
+    int *fa;
+
+public:
+    ufind(int n)
+    {
+        fa = new int[MAX_N];
+        for (int i = 0; i <= n; i++)
+            fa[i] = i;
+    }
+
+    int find(int x)
+    {
+        if (fa[x] == x)
+            return x;
+        return fa[x] = this->find(fa[x]);
+    }
+
+    void merge(int x, int y)
+    {
+        const int fx = this->find(x);
+        const int fy = this->find(y);
+
+        if (fx != fy)
+        {
+            fa[fx] = fy;
+            t[fy].merge(t[fx]);
+            delete &t[fx];
+        }
+    }
+};
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cout.tie(0);
+    std::cin.tie(0);
+
+    int n, m;
+    std::cin >> n >> m;
+    for (int i = 0; i < n; i++)
+    {
+        std::cin >> importance[i];
+        t[i].insert(importance[i]);
+    }
+
+    ufind uf(n);
+    for (int i = 0; i < m; i++)
+    {
+        int u, v;
+        std::cin >> u >> v;
+        uf.merge(u, v);
+    }
+
+    int q;
+    std::cin >> q;
+    for (int i = 0; i < q; i++)
+    {
+        char op;
+        int x, y;
+        std::cin >> op >> x >> y;
+
+        if (op == 'Q')
+            std::cout << t[x][y] << "\n";
+        if (op == 'B')
+            uf.merge(x, y);
+    }
+    return 0;
+}
